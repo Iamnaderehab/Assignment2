8bbe2df924108bc2233d2211fdbc1d5f0a26c629
Nader Ehab <naderehab881@gmail.com>
2026-03-19 16:56:25 +0200

    Uncomment path and file handling console logs
    Uncomment various console.log statements for path operations and file handling in Assignment2.js.

--- Assignment2.js
+++ Assignment2.js
@@ -7,18 +7,18 @@ const EventEmitter =require ("node:events");
 
 
 // // //Question 1
-// console.log(path.dirname(__filename));
-// console.log(path.posix.join(__dirname, "index.js"));
+console.log(path.dirname(__filename));
+console.log(path.posix.join(__dirname, "index.js"));
 
 // // //Question 2
-// // console.log(path.basename("/user/files/report.pdf "));
+console.log(path.basename("/user/files/report.pdf "));
 
 
 // // //Question 3
-// // console.log(path.posix.join("/folder","app.js"))
+console.log(path.posix.join("/folder","app.js"))
 
 // // //Question 4
-// // console.log(path.extname("/docs/readme.md "));
+console.log(path.extname("/docs/readme.md "));
 
 // // //Question 5
 console.log(path.parse("/home/app/main.js  "));
@@ -26,93 +26,93 @@ console.log({Name : path.basename("/home/app/main.js  "), Ext : path.extname("/h
 
 
 // // //Question 6
-// // console.log(path.isAbsolute("/home/user/file.txt "));
+console.log(path.isAbsolute("/home/user/file.txt "));
 
 // // //Question 7
-// // console.log(path.posix.join("src","components","App.js"));
+console.log(path.posix.join("src","components","App.js"));
 
 // // //Question 8
-// // console.log(path.resolve("./index.js "));
+console.log(path.resolve("./index.js "));
 
 // // //Question 9
-// // console.log(path.posix.join(" /folder1","folder2/file.txt "))
+console.log(path.posix.join(" /folder1","folder2/file.txt "))
 
 // // //Question 10
-// // const file= filePath=path.resolve("./data.txt");
-// // fs.unlink(file,err =>{
-// //     if(err){
-// //         console.log("cannot delete file");
-// //     }
-// //     else{
-// //         console.log( "The file.txt is deleted.");
+const file= filePath=path.resolve("./data.txt");
+fs.unlink(file,err =>{
+    if(err){
+        console.log("cannot delete file");
+    }
+    else{
+        console.log( "The file.txt is deleted.");
         
-// //     }
-// // })
+    }
+})
 
 // // //Question 11
-// // function makeFoldersync(data) {
-// //     try {
-// //         fs.mkdirSync(data);
-// //         console.log("Success");
-// //     } catch (error) {
-// //         console.log("error");
+function makeFoldersync(data) {
+    try {
+        fs.mkdirSync(data);
+        console.log("Success");
+    } catch (error) {
+        console.log("error");
     
-// // }
-// // }
-// // console.log( makeFoldersync("data.txt"));
+}
+}
+console.log( makeFoldersync("data.txt"));
 
 // //Question 12
 
-// const eventEmitter= new EventEmitter();
-// eventEmitter.on("start",()=>{
+const eventEmitter= new EventEmitter();
+eventEmitter.on("start",()=>{
 
-//     console.log(" Welcome event triggered!");
+    console.log(" Welcome event triggered!");
     
-// })
-// eventEmitter.emit("start");
+})
+eventEmitter.emit("start");
 
 // //Question 13
-// eventEmitter.on("login",name=>{
-//     console.log(`User logged in: ${name}`);
+eventEmitter.on("login",name=>{
+    console.log(`User logged in: ${name}`);
     
-// })
-// eventEmitter.emit("login","Ahmed")
+})
+eventEmitter.emit("login","Ahmed")
 
 // //Question 14
-// const file = path.resolve("./notes.txt");
-// console.log(file);
+const file = path.resolve("./notes.txt");
+console.log(file);
 
-// function readFilesync(filePath) {
-// try {
-//     const data = fs.readFileSync(filePath, 'utf8');
-//     console.log(" the file content =>" + data);
-// } 
-// catch (error) {
-//     console.error("Error")
-// }
-// }
-// readFilesync(file)
+function readFilesync(filePath) {
+try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    console.log(" the file content =>" + data);
+} 
+catch (error) {
+    console.error("Error")
+}
+}
+readFilesync(file)
 
 // //Question 15
-// const file2=path.resolve("./notes.txt")
-// fs.appendFile(file2," Async Save",error=>{
-//   if(error){
-//         console.log("error");
+const file2=path.resolve("./notes.txt")
+fs.appendFile(file2," Async Save",error=>{
+  if(error){
+        console.log("error");
         
-//     }
-//     else{
-//         console.log("Async Save");
-//     }
-// });
+    }
+    else{
+        console.log("Async Save");
+    }
+});
   
 
 // //Question 16
-// const file3=path.resolve("./notes.txt")
-// console.log(path.isAbsolute(file3));
+const file3=path.resolve("./notes.txt")
+console.log(path.isAbsolute(file3));
 
 
 // //Question 17
-// // const os= require ('node:os');
-// // console.log(os.platform());
-// // console.log(os.arch());
+const os= require ('node:os');
+console.log(os.platform());
+console.log(os.arch());
  
